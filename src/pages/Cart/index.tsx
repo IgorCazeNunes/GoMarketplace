5a798274901461c24268ff602533df6c9eacd184
Crummorn <Igorcaze@gmail.com>
2021-02-15 18:31:45 -0300

    Feat: Funcionalidade para mostrar o valor total de produtos no carrinho.

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,10 +47,16 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalQuantity = products.reduce(
+      (accumulator, { price, quantity }) => {
+        // eslint-disable-next-line no-param-reassign
+        return accumulator + price * quantity;
+      },
+      0,
+    );
 
-    return formatValue(0);
-  }, []);
+    return formatValue(totalQuantity);
+  }, [products]);
 
   const totalItensInCart = useMemo(() => {
     const totalQuantity = products.reduce((accumulator, { quantity }) => {
@@ -59,8 +65,6 @@ const Cart: React.FC = () => {
     }, 0);
 
     return totalQuantity;
-
-    return 0;
   }, [products]);
 
   return (
